e53ad22e6c3a8ab9439a32d4f11fe147f94e3aa2
Sarah Binney <sar.binney@hotmail.co.uk>
2021-07-10 16:22:17 +0100

    Click sky background to switch between images

--- gallery-hang/hang-room.js
+++ gallery-hang/hang-room.js
@@ -8,3 +8,15 @@ function scrollToSky(e) {
     e.preventDefault();
     window.scroll({top: 0, behavior: "smooth"});
 }
+
+function toggleSky(e) {
+    e.preventDefault();
+    const skyDiv = document.getElementById('hang-room-sky');
+    if (skyDiv.classList.contains('background-clouds')) {
+        skyDiv.classList.replace('background-clouds', 'background-stars');
+    } else if (skyDiv.classList.contains('background-stars')) {
+        skyDiv.classList.replace('background-stars', 'background-wash');
+    } else {
+        skyDiv.classList.replace('background-wash', 'background-clouds');
+    }
+}
