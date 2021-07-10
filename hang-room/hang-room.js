56a062801075476a8272403eb939db80b0473b3d
Sarah Binney <sar.binney@hotmail.co.uk>
2021-07-10 15:56:29 +0100

    Add "Look up" button

--- hang-room/hang-room.js
+++ hang-room/hang-room.js
@@ -2,7 +2,9 @@
 window.onload = function() {
     window.scroll({top: document.body.scrollHeight});
     document.getElementById('loading-panel').classList.add('loaded');
-    console.log("Loaded!");
 }
 
-console.log("Hello, world!");
+function scrollToSky(e) {
+    e.preventDefault();
+    window.scroll({top: 0, behavior: "smooth"});
+}
