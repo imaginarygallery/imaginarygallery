28787ef3a9f56f34b0fdacdfbbfa02e72e26fe22
Binney <sarah.binney@softwire.com>
2021-07-12 20:38:57 +0100

    Start to move menu into right configuration

--- shared/menu/menu.js
+++ shared/menu/menu.js
@@ -1,13 +1,13 @@
 function openMenu(menu) {
     console.log("Opening menu");
     menu.classList.replace('closed', 'open');
-    document.getElementById('open-menu-button').innerHTML = "Menu";
+    document.getElementById('open-menu-button').innerHTML = "Close Menu";
 }
 
 function closeMenu(menu) {
     console.log("Closing menu");
     menu.classList.replace('open', 'closed');
-    document.getElementById('open-menu-button').innerHTML = "Close";
+    document.getElementById('open-menu-button').innerHTML = "Open Menu";
 }
 
 function toggleMenu(e) {
