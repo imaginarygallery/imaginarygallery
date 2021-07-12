58a2e17ad944f4137fae9f45c23f67f418b695c1
Binney <sarah.binney@softwire.com>
2021-07-12 21:16:58 +0100

    Add animation to button

--- shared/menu/menu.js
+++ shared/menu/menu.js
@@ -1,13 +1,21 @@
 function openMenu(menu) {
     console.log("Opening menu");
     menu.classList.replace('closed', 'open');
-    document.getElementById('open-menu-button').innerHTML = "Close Menu";
+    document.getElementById('open-menu-button-text').innerHTML = 'Close Menu';
+    document.getElementById('close-menu-button-image').hidden = false;
+    document.getElementById('open-menu-button-image').hidden = true;
+    // Reset the animation of the hidden img:
+    document.getElementById('open-menu-button-image').setAttribute('src', '/shared/menu/Cross-to-Logo-onetime.gif');
 }
 
 function closeMenu(menu) {
     console.log("Closing menu");
     menu.classList.replace('open', 'closed');
-    document.getElementById('open-menu-button').innerHTML = "Open Menu";
+    document.getElementById('open-menu-button-text').innerHTML = 'Open Menu';
+    document.getElementById('open-menu-button-image').hidden = false;
+    document.getElementById('close-menu-button-image').hidden = true;
+    // Reset the animation of the hidden img:
+    document.getElementById('close-menu-button-image').setAttribute('src', '/shared/menu/Logo-to-Cross-onetime.gif');
 }
 
 function toggleMenu(e) {
